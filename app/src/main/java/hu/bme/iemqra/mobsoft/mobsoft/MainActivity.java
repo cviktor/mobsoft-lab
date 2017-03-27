2a99df191acb11ec492d046d7eb9e9b201d569bc
Czeglédi Viktor <czegledi.viktor@outlook.com>
2017-03-27 08:45:03 +0200

    Commit 2

--- app/src/main/java/hu/bme/iemqra/mobsoft/mobsoft/MainActivity.java
+++ app/src/main/java/hu/bme/iemqra/mobsoft/mobsoft/MainActivity.java
@@ -3,6 +3,7 @@ package hu.bme.iemqra.mobsoft.mobsoft;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//t
 public class MainActivity extends AppCompatActivity {
 
     @Override
